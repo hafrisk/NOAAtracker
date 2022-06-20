8a0fc3781fbd4c4f2cdb1ffeb4af3ed055b25a0e
hafrisk <56049734+hafrisk@users.noreply.github.com>
2022-06-20 14:06:17 -0500

    Add files via upload
    file to create a database to store data collected each time pulldata.py runs

---
+++ create_db.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+import sqlite3
+
+conn = sqlite3.connect('NOAA_data.sqlite')
+cur = conn.cursor()
+
+# Make some fresh tables using executescript()
+cur.executescript('''
+
+CREATE TABLE data (
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    timestamp       TIME,
+    windmph         INTEGER,
+    vis             INTEGER,
+    weather         TEXT,
+    skycond         TEXT,
+    airtemp         INTEGER,
+    Dewpoint        INTEGER,
+    sixhrmax        INTEGER,
+    sixhrmin        INTEGER,
+    relhum          INTEGER,
+    windchill       INTEGER,
+    heatindex       INTEGER,
+    p_alt           INTEGER,
+    p_sea           INTEGER,
+    onehrprecip     INTEGER,
+    threehrprecip   INTEGER,
+    sixhourprecip   INTEGER
+
+);
+''')
