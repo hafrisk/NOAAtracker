2e8f518443289cc95db3a46eb56f72e52ff5ab55
hafrisk <56049734+hafrisk@users.noreply.github.com>
2022-06-20 15:26:34 -0500

    Add files via upload
    This file uses matplotlib to create a scatter plot of all of the air temperature measurements

---
+++ line.py
@@ -0,0 +1,28 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect('NOAA_data.sqlite')
+cur = conn.cursor()
+
+cur.execute('SELECT timestamp, airtemp FROM data')
+dates = list()
+temps = list()
+for row in cur :
+    dates.append(row[0])
+    temps.append(row[1])
+
+# Plot scatter
+
+plt.scatter(dates, temps)
+
+# Labelling
+
+plt.xlabel("Date")
+plt.ylabel("Temp in Faherenheit")
+
+# Auto space
+plt.tight_layout()
+
+# Display
+plt.show()
